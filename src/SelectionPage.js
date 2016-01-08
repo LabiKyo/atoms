1e58b56f47f31b2d6e401cf10deba755ba236b19
Labi Kyo <labikyo@gmail.com>
2016-01-08 16:49:10 +0800

    Add Shake handler

--- src/SelectionPage.js
+++ src/SelectionPage.js
@@ -1,61 +1,75 @@
+import Shake from 'shake.js';
+
 import ElementBall from './ElementBall';
 import elements from './elements';
 
-function drawReactor(container) {
-  const image = queue.getResult('plus');
-  const reactor = new createjs.Bitmap(image);
+class SelectionPage {
+  constructor() {
+    this.page = new createjs.Container();
+    this.ballContainer = new createjs.Container();
 
-  reactor.x = PAGE_WIDTH / 2;
-  reactor.y = PAGE_HEIGHT / 2;
-  reactor.scaleX = 0;
-  reactor.scaleY = 0;
-  reactor.alpha = 0;
-  reactor.regX = image.width / 2;
-  reactor.regY = image.height / 2;
+    this.drawReactor();
+    this.drawElementBalls();
+    this.setupShake();
 
-  container.addChild(reactor);
-
-  createjs.Tween.get(reactor).to({
-    scaleX: 1 / 3,
-    scaleY: 1 / 3,
-    alpha: 1,
-  }, 800, createjs.Ease.quintIn())
-  .call(() => {
-    createjs.Tween.get(reactor, { loop: true })
-      .to({
-        scaleX: 7 / 24,
-        scaleY: 7 / 24,
-      }, 800, createjs.Ease.quintOut())
-      .to({
-        scaleX: 1 / 3,
-        scaleY: 1 / 3,
-      }, 800, createjs.Ease.quintIn());
-  });
-}
-
-function drawElementBalls(container) {
-  elements.reverse().forEach((element) => {
-    const ball = new ElementBall(queue.getResult(`element-ball-${element}`));
-
-    ball.on('click', () => {
-      router.navigate(`/intro/${element}`);
+    this.page.addChild(this.ballContainer);
+    stage.addChild(this.page);
+  }
+  setupShake() {
+    this.shake = new Shake({
+      threshold: 8,
+      timeout: 1000,
     });
+    this.shake.start();
+    window.addEventListener('shake', () => {
+      this.drawElementBalls();
+    }, false);
+  }
+  drawReactor() {
+    const image = queue.getResult('plus');
+    const reactor = new createjs.Bitmap(image);
 
-    container.addChild(ball);
-  });
-}
+    reactor.x = PAGE_WIDTH / 2;
+    reactor.y = PAGE_HEIGHT / 2;
+    reactor.scaleX = 0;
+    reactor.scaleY = 0;
+    reactor.alpha = 0;
+    reactor.regX = image.width / 2;
+    reactor.regY = image.height / 2;
 
-function drawPage() {
-  const page = new createjs.Container();
-  const ballContainer = new createjs.Container();
+    this.page.addChild(reactor);
 
-  drawReactor(page);
-  drawElementBalls(ballContainer);
-  page.addChild(ballContainer);
+    createjs.Tween.get(reactor).to({
+      scaleX: 1 / 3,
+      scaleY: 1 / 3,
+      alpha: 1,
+    }, 800, createjs.Ease.quintIn())
+    .call(() => {
+      createjs.Tween.get(reactor, { loop: true })
+        .to({
+          scaleX: 7 / 24,
+          scaleY: 7 / 24,
+        }, 800, createjs.Ease.quintOut())
+        .to({
+          scaleX: 1 / 3,
+          scaleY: 1 / 3,
+        }, 800, createjs.Ease.quintIn());
+    });
+  }
+  drawElementBalls() {
+    this.ballContainer.removeAllChildren();
+    elements.reverse().forEach((element) => {
+      const ball = new ElementBall(queue.getResult(`element-ball-${element}`));
 
-  stage.addChild(page);
+      ball.on('click', () => {
+        router.navigate(`/intro/${element}`);
+      });
+
+      this.ballContainer.addChild(ball);
+    });
+  }
 }
 
 export default function() {
-  drawPage();
+  return new SelectionPage();
 }
